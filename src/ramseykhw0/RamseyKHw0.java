7d0ba8c96fd5756e595c789b533c87462c5f03ca
RamseyKerley <kerleyramsey@gmail.com>
2020-01-16 19:55:18 -0600

    updated code

--- src/ramseykhw0/RamseyKHw0.java
+++ src/ramseykhw0/RamseyKHw0.java
@@ -5,6 +5,9 @@
  */
 package ramseykhw0;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author ramsey.kerley
@@ -16,6 +19,27 @@ public class RamseyKHw0 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        String[] choices = new String[3];
+        choices[0] = "sushi";
+        choices[1] = "shrimp";
+        choices[2] = "taco";
+        boolean guessing = true;
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int pointInArray = rand.nextInt(3);
+        // System.out.print(pointInArray);
+
+        while (guessing) {
+            System.out.println("The secret meal must be one of the following");
+            System.out.println(choices[0]);
+            System.out.println(choices[1]);
+            System.out.println(choices[2]);
+            System.out.print("Guess the secret meal: ");
+            String guess = input.next();
+            System.out.println();
+        }
+
     }
-    
+
 }
